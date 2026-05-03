9a139ca19728d0da0b7bd0a043ed29c3f2a6f6b2
lion-concord <lion-concord@github.com>
2026-05-03 16:28:51 +0700

    fix: Initialize VK SDK in MainActivity

--- android/app/src/main/java/ru/konconsulting/loyalty/MainActivity.java
+++ android/app/src/main/java/ru/konconsulting/loyalty/MainActivity.java
@@ -3,10 +3,15 @@ package ru.konconsulting.loyalty;
 import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
+import com.vk.api.sdk.VK;
+import com.vk.api.sdk.VKTokenExpiredHandler;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Инициализация VK SDK
+        VK.initialize(getApplicationContext());
     }
 }
