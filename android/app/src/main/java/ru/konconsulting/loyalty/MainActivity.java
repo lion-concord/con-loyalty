baa1108dc0b7bd83d978be3a7c40ee60acadd9d9
lion-concord <lion-concord@github.com>
2026-05-03 08:20:03 +0700

    fix: clean MainActivity from VK SDK code (correct path)

--- android/app/src/main/java/ru/konconsulting/loyalty/MainActivity.java
+++ android/app/src/main/java/ru/konconsulting/loyalty/MainActivity.java
@@ -1,30 +1,12 @@
 package ru.konconsulting.loyalty;
 
 import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
-import com.vk.api.sdk.VK;
-import com.vk.api.sdk.VKTokenExpiredHandler;
 
 public class MainActivity extends BridgeActivity {
-
-    private final VKTokenExpiredHandler vkTokenTracker = new VKTokenExpiredHandler() {
-        @Override
-        public void onTokenExpired() {
-            // Токен истёк, можно показать уведомление
-        }
-    };
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Инициализация VK SDK
-        VK.addTokenExpiredHandler(vkTokenTracker);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        VK.removeTokenExpiredHandler(vkTokenTracker);
     }
 }
