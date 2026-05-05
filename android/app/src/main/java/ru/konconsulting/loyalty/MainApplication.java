8c2fd40379745ec6d9a0a8f7f0321c86bb00603e
lion-concord <lion-concord@github.com>
2026-05-05 20:41:51 +0700

    Fix VKID.init(): use Companion.init(getApplicationContext())

--- android/app/src/main/java/ru/konconsulting/loyalty/MainApplication.java
+++ android/app/src/main/java/ru/konconsulting/loyalty/MainApplication.java
@@ -7,6 +7,7 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        VKID.init(this);
+        // Инициализация VK ID SDK
+        VKID.Companion.init(getApplicationContext());
     }
 }
