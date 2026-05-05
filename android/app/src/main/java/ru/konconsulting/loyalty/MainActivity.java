4a4db031d4ef759195da81f6ac69e6541dbb5c1b
lion-concord <lion-concord@github.com>
2026-05-05 17:25:00 +0700

    Remove incorrect VKID initialization from MainActivity

--- android/app/src/main/java/ru/konconsulting/loyalty/MainActivity.java
+++ android/app/src/main/java/ru/konconsulting/loyalty/MainActivity.java
@@ -3,18 +3,10 @@ package ru.konconsulting.loyalty;
 import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
-import com.vk.id.VKID;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Инициализация VK ID SDK
-        try {
-            VKID vkid = new VKID(getApplicationContext());
-        } catch (Exception e) {
-            android.util.Log.e("MainActivity", "Failed to initialize VK ID", e);
-        }
     }
 }
