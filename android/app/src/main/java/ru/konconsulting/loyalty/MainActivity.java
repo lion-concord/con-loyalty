72165c002e7b0e1045f86b04150a630742d95d58
lion-concord <lion-concord@github.com>
2026-05-05 18:12:53 +0700

    Add capacitor-vk-id module to settings.gradle and app dependencies

--- android/app/src/main/java/ru/konconsulting/loyalty/MainActivity.java
+++ android/app/src/main/java/ru/konconsulting/loyalty/MainActivity.java
@@ -3,14 +3,10 @@ package ru.konconsulting.loyalty;
 import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
-import com.conloyalty.vkid.VkIdPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Регистрация VK ID плагина
-        registerPlugin(VkIdPlugin.class);
     }
 }
